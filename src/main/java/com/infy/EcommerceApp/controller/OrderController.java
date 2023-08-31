d6720e93b155d9c70bacc78351a115c71ec8fada
Alex4ndro5 <alek.folfas@gmail.com>
2023-08-31 15:22:55 +0530

    Spring security

--- src/main/java/com/infy/EcommerceApp/controller/OrderController.java
+++ src/main/java/com/infy/EcommerceApp/controller/OrderController.java
@@ -11,15 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
+@RequestMapping("api")
 public class OrderController {
     @Autowired
     OrderRepository orderRepository;
