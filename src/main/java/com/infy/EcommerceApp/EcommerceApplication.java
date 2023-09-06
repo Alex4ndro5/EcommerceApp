13084c8eaa29b590262451cb777754d257208312
sofiacasalini <sofia.casalini@gmail.com>
2023-09-06 15:21:13 +0530

    Aspect

--- src/main/java/com/infy/EcommerceApp/EcommerceApplication.java
+++ src/main/java/com/infy/EcommerceApp/EcommerceApplication.java
@@ -11,8 +11,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class EcommerceApplication {
 	public EcommerceApplication() {
 	}
--- src/main/java/com/infy/EcommerceApp/logModule/LoggerAspect.java
+++ src/main/java/com/infy/EcommerceApp/logModule/LoggerAspect.java
@@ -1,23 +1,48 @@
 package com.infy.EcommerceApp.logModule;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-@Component
 @Aspect
+@Component
 
 public class LoggerAspect {
     private static final Logger logger = LogManager.getLogger(LoggerAspect.class.getName());
 
-    @After(value="execution(* com.infy.EcommerceApp.GetMapping)")
-        public void afterAdvice(JoinPoint jp){
-        logger.info();
-    }
+    @Pointcut(value="execution(* com.infy.EcommerceApp.controller.*.*(..) )")
+    public void myPointCut () {
     }
 
+    @AfterReturning("myPointCut()")
+    public void applicationLogger(JoinPoint jp) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        String className = jp.getTarget().getClass().toString();
+        String methodName = jp.getSignature().getName();
+        Object[] array = jp.getArgs();
+
+        logger.info(className + methodName + mapper.writeValueAsString(array));
+
+    }
+
+    /*@AfterThrowing("myPointCut()")
+    public void exceptionsLogger(JoinPoint jp, Exception ex){
+
+        String className = jp.getTarget().getClass().toString();
+        String methodName = jp.getSignature().getName();
+*/
+    }
 
 
 
@@ -25,4 +50,7 @@ public class LoggerAspect {
 
 
 
-}
+
+
+
+
